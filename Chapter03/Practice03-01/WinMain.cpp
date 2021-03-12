472e9c772d2f1e872d206f35dd95085fc0993728
yeqfx <yeqfx@hotmail.com>
2021-03-12 15:54:17 +0900

    3장 제어 메시지 처리하기

---
+++ Chapter03/Practice03-01/WinMain.cpp
@@ -0,0 +1,73 @@
+#include <Windows.h>
+#include <tchar.h>
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM IParam);
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow)
+{
+    HWND hwnd;
+    MSG msg;
+    WNDCLASS WndClass;
+    WndClass.style = CS_HREDRAW | CS_VREDRAW;
+    WndClass.lpfnWndProc = WndProc;
+    WndClass.cbClsExtra = 0;
+    WndClass.cbWndExtra = 0;
+    WndClass.hInstance = hInstance;
+    WndClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+    WndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    WndClass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+    WndClass.lpszMenuName = NULL;
+    WndClass.lpszClassName = _T("Window Class Name");
+    //WndClass.hIconSm = LoadIcon(WndClass.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+    RegisterClass(&WndClass);
+
+    hwnd = CreateWindow(L"Window Class Name",
+        _T("Window Title Name"),
+        WS_OVERLAPPEDWINDOW,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        CW_USEDEFAULT,
+        NULL,
+        NULL,
+        hInstance,
+        NULL
+    );
+    ShowWindow(hwnd, nCmdShow);
+    UpdateWindow(hwnd);
+
+    while (GetMessage(&msg, NULL, 0, 0))
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+    return (int)msg.wParam;
+}
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+    HDC hdc;
+    PAINTSTRUCT ps;
+    static int x, y;
+
+    switch (iMsg)
+    {
+    case WM_CREATE:
+        x = 20, y = 20;
+        break;
+    case WM_PAINT:
+        hdc = BeginPaint(hwnd, &ps);
+        Ellipse(hdc, x - 20, y - 20, x + 20, y + 20);
+        EndPaint(hwnd, &ps);
+        break;
+    case WM_KEYDOWN:
+        if (wParam == VK_RIGHT) x += 40;
+        InvalidateRgn(hwnd, NULL, TRUE);
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    }
+    return DefWindowProc(hwnd, iMsg, wParam, lParam);
+}
